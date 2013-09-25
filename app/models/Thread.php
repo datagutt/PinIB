e939e0689082cd5e33b284c667713e08876c2b62
datagutt <datagutt@lekanger.no>
2013-09-25 17:34:57 +0200

    Load threads from DB

--- app/models/Thread.php
+++ app/models/Thread.php
@@ -3,7 +3,7 @@ namespace PinIB\Models;
 class Thread extends \PinIB\Model{
 	protected $table = 'threads';
 	
-	public function  posts($amount = 10){
+	public function posts($amount = 10){
 		$query = new \PinIB\SQLQuery($this->table);
 		if($cache = $this->redis->get('threads')){
 			$threads = unserialize($cache);
