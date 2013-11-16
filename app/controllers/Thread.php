41ce253c8f6a8d814da2d410991e3fcd14f93327
datagutt <datagutt@lekanger.no>
2013-11-16 15:21:20 +0100

    Fixes

--- app/controllers/Thread.php
+++ app/controllers/Thread.php
@@ -16,7 +16,24 @@ class Thread extends \PinIB\Controller{
 	public function newthread(){
 		$thread = $this->app->getModel('thread');
 		
-		$thread->newThread('title', 'http://', 399, 399, 1, $isAnon);
+		if(Input::post('thread-content')){
+			\PinIB\CSRF::check();
+			
+			$thread_content = strip_tags(Input::post('thread-content'));
+			if($image = Input::post('image')){
+				if(\PinIB\Auth::guest()){
+					$isAnon = 1;
+					$uid = 0;
+				}else{
+					$isAnon = Input::post('anon') ? Input::post('anon') == 'on' : false;
+					$uid = $_SESSION['user']['id'];
+				}
+				
+				$thread->newThread($title, $thread_content, $image, 399, 399, $uid, $isAnon);
+			}else{
+				// Tell the user that threads MUST include an image
+			}
+		}
 	}
 	
 	public function reply($slug = ''){
